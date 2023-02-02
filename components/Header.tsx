2586ecad1d38c5f12e707526c23fb0b154ff7342
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-02-02 23:48:28 +0500

    FEAT: Fine tuning navigation bar design

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,20 +11,32 @@ import {
   Image,
   Card,
   CardBody,
+  chakra,
+  Button,
+  Link,
+  HStack,
 } from "@chakra-ui/react";
 
+const CTA = "Get Started";
+
 export default function Header() {
+  const data = ["About", "Contact Us", "Web3"];
   return (
-    <Stack direction={["column", "row"]} spacing="24px">
-      <Flex>
-        <Image
-          borderRadius="full"
-          boxSize="150px"
-          src="assets/panaverse_dao_logo.jpeg"
-          alt="Panaverse DAO"
-        />
+    <chakra.header id="header">
+      <Flex w="100%" px="6" py="5" align="center" justify="space-between">
+        <Image src="assets/panaverse-logo/panaverse80_80.jpg" h="50px" alt="Panaverse DAO"/>
+
+        <HStack as="nav" spacing="5">
+          {data.map((item: any, i: number) => (
+            <Link key={i}>
+              <Button variant="nav"> {item} </Button>
+            </Link>
+          ))}
+        </HStack>
+        <HStack>
+          <Button>{CTA}</Button>
+        </HStack>
       </Flex>
-      <Flex></Flex>
-    </Stack>
+    </chakra.header>
   );
 }
