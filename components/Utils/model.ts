e2238ede380c5383368cbf2094db97388cd2bf26
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-02-23 22:49:38 +0500

    REF: added routes

--- components/Utils/model.ts
+++ components/Utils/model.ts
@@ -2,4 +2,5 @@ export interface Course {
     name: string;
     description: string;
     imageUrl?: string;
+    route?: string;
 }
--- components/Utils/util.tsx
+++ components/Utils/util.tsx
@@ -90,31 +90,37 @@ export function transformText(text: string) {
 export const speciliazed_courses: Array<Course> =
   [
     {
+      route: "web3_metaverse",
       name: "Web 3.0 (Blockchain) and Metaverse Specialization",
       description:
         "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.",
     },
     {
+      route: "artificial_intelligence",
       name: "Artificial Intelligence (AI) and Deep Learning Specialization",
       description:
         "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
     },
     {
+      route: "cloud_computing",
       name: "Cloud-Native Computing Specialization",
       description:
         "The Cloud-Native Computing Specialization focuses on Containers, Kubernetes, and CDK for Kubernetes.",
     },
     {
+      route: "ambient_computing_iot",
       name: "Ambient Computing and IoT Specialization",
       description:
         "The Ambient Computing and IoT Specialization focuses on building Smart Homes, Offices, Factories, and Cities using Voice computing, Matter Protocol, and Embedded Devices.",
     },
     {
+      route: "genomics_bioinformatics",
       name: "Genomics and Bioinformatics Specialization",
       description:
         "Genomics is the study of the total genetic makeup of individual organisms, and how this genetic information is structured, functions, and has evolved; bioinformatics encompasses a diverse range of analytical methods and tools applied to genomic data.",
     },
     {
+      route: "network_programmability_automation",
       name: "Network Programmability and Automation Specialization",
       description:
         "More than ever, network engineers are finding it challenging to complete their duties entirely manually. Network automation is now crucial due to new protocols, technologies, and delivery models.",
