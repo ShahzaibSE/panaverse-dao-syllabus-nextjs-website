893d4ee46ca6500cf84e0de931b8297e6866cb9a
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-02-25 00:38:24 +0500

    FEAT: Scaling course detailing component

--- components/Utils/courseDetails.tsx
+++ components/Utils/courseDetails.tsx
@@ -1,18 +1,26 @@
-'use client';
-import {useState} from "react";
+"use client";
+import { useState } from "react";
 import {
   DrawerBody,
   DrawerHeader,
-  DrawerOverlay,Drawer, DrawerCloseButton, DrawerContent,
-  Button, useDisclosure
+  DrawerOverlay,
+  Drawer,
+  DrawerCloseButton,
+  DrawerContent,
+  Box,
+  Flex,
+  Text,
+  Heading,
+  Card,
+  CardBody,
+  Image
 } from "@chakra-ui/react";
 import { InfoIcon } from "@chakra-ui/icons";
 import { Course } from "./model";
-
-
+import { Chrono } from "react-chrono";
 
 export default function CourseDetails(props: any) {
-  const {name, description} = props.course;
+  const { name, description } = props.course;
 
   return (
     <>
@@ -20,14 +28,74 @@ export default function CourseDetails(props: any) {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerHeader>{name}</DrawerHeader>
+          <DrawerHeader>
+            {" "}
+            <Heading textAlign={"justify"} fontSize={["lg", "xl", "2xl"]}>
+              {name}
+            </Heading>
+          </DrawerHeader>
           <DrawerBody>
-            <p>
-             {description}
-            </p>
+            <Flex direction="column" justify="center">
+              <Text
+                as="p"
+                textAlign={["justify"]}
+                fontSize={["xs", "sm", "md", "lg"]}
+              >
+                {description}
+              </Text>
+              <Box>
+                <Chrono mode="VERTICAL">
+                  <div>
+                    <div style={{ width: "250px", height: "250px" }}>
+                      <Image
+                        style={{ maxWidth: "100%", maxHeight: "100%" }}
+                        alt="test"
+                        src="https://cdn.tutsplus.com/net/uploads/2013/08/github-collab-retina-preview.gif"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <h3>This is a List</h3>
+                    <ul>
+                      <li>Item 1</li>
+                      <li>Item 2</li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h3>Dunkirk</h3>
+                    <p>
+                      The Battle of Dunkirk (French: Bataille de Dunkerque) was
+                      fought in Dunkirk (Dunkerque), France, during the Second
+                      World War, between the Allies and Nazi Germany.
+                    </p>
+                  </div>
+                  <div style={{ margin: "1rem" }}>
+                    <h3>Table</h3>
+                    <table>
+                      <thead>
+                        <tr>
+                          <td>Column 1</td>
+                          <td>Column 2</td>
+                          <td>Column 3</td>
+                          <td>Column 4</td>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Value 1</td>
+                          <td>Value 2</td>
+                          <td>Value 3</td>
+                          <td>Value 4</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </Chrono>
+              </Box>
+            </Flex>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
     </>
-  )
+  );
 }
